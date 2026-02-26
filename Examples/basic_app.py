976512dbd08bbbd33bb2c798e7b959c0eb49e80d
ty3370 <77711112+ty3370@users.noreply.github.com>
2026-02-26 13:05:39 +0900

    Add files via upload

--- Examples/basic_app.py
+++ Examples/basic_app.py
@@ -46,16 +46,13 @@ st.subheader("💬 대화 로그")
 
 st.markdown("""
 <style>
-[data-testid="stVerticalBlock"] {
-    background-image: url("https://i.imgur.com/L8T0tdU.jpg");
+[data-testid="stVerticalBlock"] div:has(> div[data-testid="stChatMessage"]) {
+    background-image: url("https://i.imgur.com/8epnNuh.png");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
     background-attachment: local;
-}
-
-[data-testid="stChatMessage"] {
-    background-color: rgba(255,255,255,0.85);
+    opacity: 0.95;
 }
 </style>
 """, unsafe_allow_html=True)
