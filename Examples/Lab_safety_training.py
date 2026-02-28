7774632ea13fcbf0f3feaf6efa6e2f8ef7b0561b
ty3370 <77711112+ty3370@users.noreply.github.com>
2026-02-28 13:09:36 +0900

    Add files via upload

--- Examples/Lab_safety_training.py
+++ Examples/Lab_safety_training.py
@@ -148,7 +148,7 @@ def page_intro():
     with col_center:
 
         st.markdown(
-            "<h2 style='text-align: center;'>🧪 보라고등학교<br>실험실 폭발 사고 조사<br></h2>",
+            "<h2 style='text-align: center;'>🧪 보라고등학교<br>실험실 폭발 사고 조사</h2>",
             unsafe_allow_html=True
         )
 
@@ -162,7 +162,7 @@ def page_intro():
         st.markdown("""
         <div style='text-align: center;'>
 
-        #### <br>🕵️ 활동 안내
+        #### 🕵️ 활동 안내
 
         20xx년 x월 x일, 보라고등학교 실험실에서 폭발 사고가 발생했습니다. 빠른 대피와 교사의 적절한 조치로 인명 피해는 없었지만, 교육청에서는 사안 조사를 위해 조사관을 파견했습니다.
 
